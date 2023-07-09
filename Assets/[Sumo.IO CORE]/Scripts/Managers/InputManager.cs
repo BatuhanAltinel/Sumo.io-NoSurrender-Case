6da5f2948bc938e9c8ca8d5df35c676ee045f540
Batuhan ALTINEL <batuhan.altinel@hotmail.com>
2023-07-10 02:02:07 +0300

    Update InputManager.cs

--- Assets/[Sumo.IO CORE]/Scripts/Managers/InputManager.cs
+++ Assets/[Sumo.IO CORE]/Scripts/Managers/InputManager.cs
@@ -3,34 +3,33 @@ using UnityEngine;
 public class InputManager : Singleton<InputManager>
 {
 
-	public bool InputEnable { get; set; }
+  public bool InputEnable { get; set; }
 
-	void Start()
-	{
-		InputEnable = true;
-	}
+  void Start()
+  {
+    InputEnable = true;
+  }
 
-	void Update()
-	{
-		Inputs();
-	}
+  void Update()
+  {
+    Inputs();
+  }
 
-	void Inputs()
-	{
-		if(Input.GetMouseButton(0) && InputEnable)
-		{
+  void Inputs()
+  {
+    if(Input.GetMouseButton(0) && InputEnable)
+    {
 
-			if((GameManager.Instance.IsState(GameState.ReadyToStartGame)))
-			{
-				EventManager.OnGameStateChanged.Invoke(GameState.OnTimer);
-			}
-			else if(GameManager.Instance.IsState(GameState.End))
-			{
-				EventManager.OnGameStateChanged.Invoke(GameState.ReadyToStartGame);
-			}
-			
-
-		}
-	}
+      if((GameManager.Instance.IsState(GameState.ReadyToStartGame)))
+      {
+	EventManager.OnGameStateChanged.Invoke(GameState.OnTimer);
+      }
+      else if(GameManager.Instance.IsState(GameState.End))
+      {
+	EventManager.OnGameStateChanged.Invoke(GameState.ReadyToStartGame);
+      }
+      
+     }
+   }
 
 }
